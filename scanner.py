d67a5872a204c6ac6d91c24b8d38177f4b22ecb6
mohmmedahmmedali1 <mohmmedahmmed77866@gmail.com>
2025-09-02 18:02:24 +0300

    scanner.py

---
+++ scanner.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+from scapy.all import ARP, Ether, srp
+import argparse
+
+def welcome_message():
+    ascii_banner = r"""
+ __  __       _                     _     _             
+|  \/  |     | |                   | |   | |            
+| \  / | ___ | |__   ___  _ __   __| | __| | ___  _ __  
+| |\/| |/ _ \| '_ \ / _ \| '_ \ / _` |/ _` |/ _ \| '_ \ 
+| |  | | (_) | | | | (_) | | | | (_| | (_| | (_) | | | |
+|_|  |_|\___/|_| |_|\___/|_| |_|\__,_|\__,_|\___/|_| |_|
+
+              🐍  محمد المشرع  🐍
+    """
+
+    ascii_snake = r"""
+          /^\/^\
+        _|__|  O|
+\/     /~     \_/ \
+ \____|__________/  \
+        \_______      \
+                `\     \                 \
+                  |     |                  \
+                 /      /                    \
+                /     /                       \
+              /      /                         \ \
+             /     /                            \  \
+           /     /             _----_            \   \
+          /     /           _-~      ~-_         |   |
+         (      (        _-~    _--_    ~-_     _/   |
+          \      ~-____-~    _-~    ~-_    ~-_-~    /
+            ~-_           _-~          ~-_       _-~
+               ~--______-~                ~-___-~
+    """
+
+    print("=" * 60)
+    print(ascii_banner)
+    print(ascii_snake)
+    print("=" * 60)
+    print("📡 الأداة ستساعدك على استعراض الأجهزة المتصلة بشبكتك.\n")
+
+
+def scan_network(ip_range):
+    arp = ARP(pdst=ip_range)
+    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+    packet = ether/arp
+    result = srp(packet, timeout=2, verbose=0)[0]
+
+    devices = []
+    for sent, received in result:
+        devices.append({'ip': received.psrc, 'mac': received.hwsrc})
+
+    return devices
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Network Scanner using Scapy")
+    parser.add_argument("range", help="Target IP range (e.g. 192.168.1.1/24)")
+    args = parser.parse_args()
+
+    welcome_message()
+
+    print(f"[+] Scanning network: {args.range}")
+    devices = scan_network(args.range)
+
+    print("\nAvailable devices in the network:")
+    print("IP Address\t\tMAC Address")
+    print("-" * 40)
+    for device in devices:
+        print(f"{device['ip']}\t\t{device['mac']}")
+
+
+if __name__ == "__main__":
+    main()
